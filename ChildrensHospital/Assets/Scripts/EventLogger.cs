641aeda06c6e6bb113a1cb076b652c18b7a2df8f
hazlett <hazlett.206@gmail.com>
2014-09-26 10:12:46 -0400

    Fixed surface area bug

--- ChildrensHospital/Assets/Scripts/EventLogger.cs
+++ ChildrensHospital/Assets/Scripts/EventLogger.cs
@@ -21,6 +21,10 @@ public class EventLogger  {
 
     private EventLogger()
     {
+        if (!System.IO.Directory.Exists(path))
+        {
+            System.IO.Directory.CreateDirectory(path);
+        }
         logFile = new FileStream(path + "\\ReachVolume.log", FileMode.Append);
         logger = new StreamWriter(logFile);
 
