e2bc906c98ab3e1532920a377a1d919aa4e8bd69
hazlett <hazlett.206@gmail.com>
2014-11-10 13:07:15 -0500

    Trunk Displacement

--- ChildrensHospital/Assets/Scripts/VolumeTracker.cs
+++ ChildrensHospital/Assets/Scripts/VolumeTracker.cs
@@ -19,21 +19,25 @@ public class VolumeTracker {
     public BoxStates BoxStateRight { get { return boxStateRight; } }
     private float lowerXLeft, lowerXRight, middleXLeft, middleXRight, upperXLeft, upperXRight,
         lowerZLeft, lowerZRight, middleZLeft, middleZRight, upperZLeft, upperZRight,
-        yLeft, yRight;
+        yLeft, yRight, trunkLeft, trunkRight, trunkForward;
     internal float lowerFarLeft, middleFarLeft, upperFarLeft, lowerFrontLeft, middleFrontLeft, upperFrontLeft, topLeft,
         lowerFarRight, middleFarRight, upperFarRight, lowerFrontRight, middleFrontRight, upperFrontRight, topRight,
         totalSurfaceArea;
     private float lowerBound = 0.1f, upperBound = 0.3f;
     private Matrix4x4 transformMatrix;
-    private int leftHandJoint = 7, rightHandJoint = 11;
-    private Vector3 leftHandPosition, rightHandPosition;
+    private int leftHandJoint = 7, rightHandJoint = 11, trunkJoint = 20;
+    private Vector3 leftHandPosition, rightHandPosition, trunkPosition;
     private bool trackRight, trackLeft;
     private Vector3 MeterToCentimeter = new Vector3(100.0f, 100.0f, 100.0f);
     private Volumes volumes;
     private Vector3 offset;
     private Vector3 rightHandAxis = new Vector3(1.0f, 1.0f, -1.0f);
     private Vector3 leftHandAxis = new Vector3(-1.0f, 1.0f, -1.0f);
+    private Vector3 trunkAxis = new Vector3(1.0f, 1.0f, -1.0f);
     private string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\ACTIVE-seated";
+    public string TrunkLeft { get { return (trunkLeft * -1.0f).ToString("G8"); } }
+    public string TrunkRight { get { return trunkRight.ToString("G8"); } }
+    public string TrunkForward { get { return trunkForward.ToString("G8"); } }
 	public VolumeTracker (float ulnaLength) {
         kinect = GameObject.Find("KinectManager").GetComponent<KinectManager>();
         transformMatrix = GameControl.Instance.TransformMatrix;
@@ -47,15 +51,6 @@ public class VolumeTracker {
             lowerBound = upperBound / 3.0f;
         }
 	}
-    public VolumeTracker(bool trackLeft, bool trackRight)
-    {
-        kinect = GameObject.Find("KinectManager").GetComponent<KinectManager>();
-        transformMatrix = GameControl.Instance.TransformMatrix;
-        volumes = new Volumes();
-        offset = new Vector3(0.0f, -0.01f, 0.30f);
-        this.trackLeft = trackLeft;
-        this.trackRight = trackRight;
-    }
 	
     private void Initialize()
     {
@@ -81,6 +76,7 @@ public class VolumeTracker {
         TranslatePositions();
         TrackLeft();
         TrackRight();
+        TrackTrunk();
         if (DebugMode)
             DebugPositions();
 	}
@@ -124,7 +120,7 @@ public class VolumeTracker {
                 + "TotalVolume, LowerLeftVolume, LowerRightVolume, MiddleLeftVolume, MiddleRightVolume, UpperLeftVolume, UpperRightVolume,"
                 + "TotalSurfaceArea, LowerFarLeft, LowerFarRight, MiddleFarLeft, MiddleFarRight, UpperFarLeft, UpperFarRight, LowerFrontLeft, LowerFrontRight, MiddleFrontLeft, MiddleFrontRight, UpperFrontLeft, UpperFrontRight,"
                 + "LowerXLeft, LowerXRight, MiddleXLeft, MiddleXRight, UpperXLeft, UpperXRight, LowerZLeft, LowerZRight, MiddleZLeft, MiddleZRight, UpperZLeft, UpperZRight,"
-                + "LowerY, MiddleY, LeftYReach, RightYReach, CalibrationMatrix, XOffset, YOffset, ZOffset");
+                + "LowerY, MiddleY, LeftYReach, RightYReach, TrunkLeft, TrunkRight, TrunkForward, CalibrationMatrix, XOffset, YOffset, ZOffset");
             }
             foreach(string line in lines)
             {
@@ -156,6 +152,7 @@ public class VolumeTracker {
                 + upperZLeft.ToString("G8") + "," + upperZRight.ToString("G8") + "," 
                 + lowerBound.ToString("G2") + "," + (upperBound - lowerBound).ToString("G2") + ","  
                 + yLeft.ToString("G8") + "," + yRight.ToString("G8") + ","
+                + TrunkLeft + "," + TrunkRight + "," + TrunkForward + ","
                 + MatrixToString(transformMatrix) + ","
                 + offset.x.ToString("G4") + "," + offset.y.ToString("G4") + "," + offset.z.ToString("G4"));
         }
@@ -207,11 +204,13 @@ public class VolumeTracker {
     {
         Debug.Log("leftHand: " + leftHandPosition);
         Debug.Log("rightHand: " + rightHandPosition);
+        Debug.Log("trunk: " + trunkPosition);
     }
     private void SetPositions()
     {
         leftHandPosition = kinect.GetJointKinectPosition(kinect.GetPrimaryUserID(), leftHandJoint);
         rightHandPosition = kinect.GetJointKinectPosition(kinect.GetPrimaryUserID(), rightHandJoint);
+        trunkPosition = kinect.GetJointKinectPosition(kinect.GetPrimaryUserID(), trunkJoint);
     }
     private void TranslatePositions()
     {
@@ -221,6 +220,24 @@ public class VolumeTracker {
         rightHandPosition = transformMatrix.MultiplyPoint3x4(rightHandPosition);
         rightHandPosition -= offset;
         rightHandPosition = Vector3.Scale(rightHandPosition, rightHandAxis);
+        trunkPosition = transformMatrix.MultiplyPoint3x4(trunkPosition);
+        trunkPosition -= offset;
+        trunkPosition = Vector3.Scale(trunkPosition, trunkAxis);
+    }
+    private void TrackTrunk()
+    {
+        if ((trunkPosition.x > 0) && (trunkPosition.x > trunkRight))
+        {
+            trunkRight = trunkPosition.x;
+        }
+        if ((trunkPosition.x < 0) && (trunkPosition.x < trunkLeft))
+        {
+            trunkLeft = trunkPosition.x;
+        }
+        if ((trunkPosition.z > trunkForward))
+        {
+            trunkForward = trunkPosition.z;
+        }
     }
     private void TrackRight()
     {
