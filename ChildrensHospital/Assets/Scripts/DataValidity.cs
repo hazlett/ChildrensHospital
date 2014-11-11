5cdcd029b9f86d56ba18c1085c1e97d55d9a894c
hazlett <hazlett.206@gmail.com>
2014-11-11 13:05:59 -0500

    Used standard deviation for validation. Added trunk data and scaled for brooke scale.

--- ChildrensHospital/Assets/Scripts/DataValidity.cs
+++ ChildrensHospital/Assets/Scripts/DataValidity.cs
@@ -16,9 +16,17 @@ public class DataValidity {
     {
         if (volumes.Count == 0)
         {
-            if (Approximately(GameControl.Instance.totalVolume, DocumentManager.Instance.PredictedVolume, 0.25f, true))
+            //if (Approximately(GameControl.Instance.totalVolume, DocumentManager.Instance.PredictedVolume, 0.25f, true))
+            //{
+            //   return null;
+            //}
+            //else
+            //{
+                //return Languages.Instance.GetTranslation("Your volume is") + " " + ((GameControl.Instance.totalVolume / DocumentManager.Instance.PredictedVolume) * 100).ToString("F1") + "% " + Languages.Instance.GetTranslation("of the predicted volume");
+            //}
+            if (CheckDeviation())
             {
-               return null;
+                return null;
             }
             else
             {
@@ -38,15 +46,53 @@ public class DataValidity {
             {
                 exception += Languages.Instance.GetTranslation("Your volume is") + " " + ((GameControl.Instance.totalVolume / averageVolume) * 100).ToString("F1") + "% " + Languages.Instance.GetTranslation("of your previous average volumes");
             }
-            if (!Approximately(GameControl.Instance.totalVolume, DocumentManager.Instance.PredictedVolume, 0.25f, true))
+            //if (!Approximately(GameControl.Instance.totalVolume, DocumentManager.Instance.PredictedVolume, 0.25f, true))
+            //{
+            //    exception += Languages.Instance.GetTranslation("Your volume is") + " " + ((GameControl.Instance.totalVolume / DocumentManager.Instance.PredictedVolume) * 100).ToString("F1") + "% " + Languages.Instance.GetTranslation("of the predicted volume");         
+            //}
+            if (!CheckDeviation())
             {
-                exception += Languages.Instance.GetTranslation("Your volume is") + " " + ((GameControl.Instance.totalVolume / DocumentManager.Instance.PredictedVolume) * 100).ToString("F1") + "% " + Languages.Instance.GetTranslation("of the predicted volume");         
+                exception += Languages.Instance.GetTranslation("Your volume is") + " " + ((GameControl.Instance.totalVolume / DocumentManager.Instance.PredictedVolume) * 100).ToString("F1") + "% " + Languages.Instance.GetTranslation("of the predicted volume");
             }
             return exception;
         }
         else
         {
             return Languages.Instance.GetTranslation("Unknown error occurred while validating the volume data");
+        }
+    }
+    private bool CheckDeviation()
+    {
+        switch(UserContainer.Instance.UserDictionary[UserContainer.Instance.currentUser].BrookeScale)
+        {
+            case 0:
+                {
+                    return Approximately(GameControl.Instance.totalVolume, 180.6f, 28.1f, false);
+                }
+            case 1:
+                {
+                    return Approximately(GameControl.Instance.totalVolume, 133.2f, 25.6f, false);
+                }
+            case 2:
+                {
+                    return Approximately(GameControl.Instance.totalVolume, 97.5f, 24.0f, false);
+                }
+            case 3:
+                {
+                    return Approximately(GameControl.Instance.totalVolume, 62.4f, 18.4f, false);
+                }
+            case 4:
+                {
+                    return Approximately(GameControl.Instance.totalVolume, 31.0f, 6.7f, false);
+                }
+            case 5:
+                {
+                    return Approximately(GameControl.Instance.totalVolume, 10.1f, 4.8f, false);
+                }
+            default:
+                {
+                    return false;
+                }
         }
     }
     private bool Approximately(float value, float min, float max)
@@ -61,7 +107,7 @@ public class DataValidity {
         }
         else
         {
-            return ((value >= value - check) && (value <= value + check));
+            return ((value >= compareValue - check) && (value <= compareValue + check));
         }
         
     }
